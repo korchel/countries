b08b64b47c2720f43328214e3aae67addc7cc44a
Elena Korchuganova <korchel21@gmail.com>
2024-01-26 21:20:02 +0300

    card style edited

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import type { ICountryCardProps } from '../types/types';
 
 const CountryCard: React.FC<ICountryCardProps> = ({ name, flag, population, region, capital }) => {
-  console.log(flag);
   return (
-    <div className="countryCard">
-      <div className="img-container">
-        <img className="flag" src={flag.svg} alt={flag.alt} />
+    <Link to={`/country/${name}`}>
+      <div className="countryCard">
+        <div className="img-container">
+          <img className="flag" src={flag.svg} alt={flag.alt} />
+        </div>
+        <div className="description">
+          <h3>{name}</h3>
+          <p><span>Population:</span>{' '}{population}</p>
+          <p><span>Region:</span>{' '}{region}</p>
+          <p><span>Capital:</span>{' '}{capital}</p>
+        </div>
       </div>
-      <div className="description">
-        <h2>{name}</h2>
-        <p><span>Population:</span>{' '}{population}</p>
-        <p><span>Region:</span>{' '}{region}</p>
-        <p><span>Capital:</span>{' '}{capital}</p>
-      </div>
-    </div>
+    </Link>
   );
 };
 
