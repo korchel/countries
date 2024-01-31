c3468c4b360273324bb8749ed55893508db148de
Elena Korchuganova <korchel21@gmail.com>
2024-01-31 16:33:13 +0300

    country path adited

--- src/paths.tsx
+++ src/paths.tsx
@@ -2,7 +2,7 @@ const apiPath = 'https://restcountries.com/v3.1';
 
 const paths = {
   all: (): string => [apiPath, 'all'].join('/'),
-  country: (name: string): string => [apiPath, 'name', name].join('/'),
+  country: (name: string): string => [apiPath, 'name', name].join('/') + '?fullText=true',
   neighbors: (countryCodes: string[]): string => [apiPath, 'alpha?codes='].join('/') + countryCodes.join(','),
 };
 
